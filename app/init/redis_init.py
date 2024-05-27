bee8b29a8ad60f95b8b90e13b3967336c1df278e
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-05-27 17:34:17 +0530

    added password

--- app/init/redis_init.py
+++ app/init/redis_init.py
@@ -4,8 +4,10 @@ import os
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
+
 # REDIS_HOST = 'localhost'
 # REDIS_PORT = 6379
 # REDIS_DB = 0
 
-redis_init = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+redis_init = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD)
