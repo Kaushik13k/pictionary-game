ab6da2f28bf708399f98572972e4a6de8098676b
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-05-27 17:30:28 +0530

    added envs

--- app/init/redis_init.py
+++ app/init/redis_init.py
@@ -1,9 +1,11 @@
 import redis
-# REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-# REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-# REDIS_DB = int(os.getenv("REDIS_DB", 0))
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 0
+import os
 
-redis_init = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_DB = 0
+
+redis_init = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
