07b3e10c9a55b817f23648b1cf13c3322d3ff61c
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-05-28 19:57:27 +0530

    code modified for create_room

--- services/create_room.py
+++ services/create_room.py
@@ -1,22 +1,52 @@
 import json
 import logging
-from services.socket_event import SocketEvent
+import traceback
+
 from init.redis_init import redis_init
+from services.socket_event import SocketEvent
+from exceptions.exceptions import CreateRoomException
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class CreateRoom(SocketEvent):
     async def handle(self, sio: str, socket_id: str, username: str):
-        logger.info(f"Handling create_room event for socket_id {socket_id} and username {username}")
-        username = json.loads(username)
-        user_data = {"creator": username["userName"], "members": [username["userName"]]}
-        redis_key = f"room_id:{socket_id}"
-        redis_init.set(redis_key, json.dumps(user_data))
-        logger.info(f"Set data in Redis for key {redis_key}")
-        await sio.emit(
-            "create_room",
-            {"room_id": socket_id, "creator": username["userName"]},
-            room=socket_id,
-        )
-        logger.info(f"Emitted create_room event for room_id {socket_id}")
+        try:
+            logger.info(
+                f"Handling create_room event for socket_id {socket_id} and username {username}"
+            )
+            username = json.loads(username)
+            user_data = {
+                "creator": username["userName"],
+                "members": [username["userName"]],
+            }
+            redis_key = f"room_id:{socket_id}"
+            result = redis_init.execute_command(
+                "JSON.SET", redis_key, "$", json.dumps(user_data)
+            )
+            if result.decode("utf-8") == "OK":
+                print("Inserted in Redis")
+                logger.info(f"Set data in Redis for key {redis_key}")
+                await sio.emit(
+                    "create_room",
+                    {"room_id": socket_id, "creator": username["userName"]},
+                    room=socket_id,
+                )
+                logger.info(f"Emitted create_room event for room_id {socket_id}")
+            else:
+                raise CreateRoomException(
+                    msg=f"Error creating room details in Redis for key {redis_key}"
+                )
+        except Exception as e:
+            logger.error(e)
+            logger.error(traceback.format_exc())
+            await sio.emit(
+                "create_room",
+                {
+                    "status": "failure",
+                    "room_id": socket_id,
+                    "message": f"Error creating room",
+                },
+                room=socket_id,
+            )
--- services/join_room.py
+++ services/join_room.py
@@ -9,20 +9,34 @@ from exceptions.exceptions import JoinRoomException
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class JoinRoom(SocketEvent):
     async def handle(self, sio: str, socket_id: str, username: str):
         try:
-            logger.info(f"Handling join_room event for socket_id {socket_id} and username {username}")
+            logger.info(
+                f"Handling join_room event for socket_id {socket_id} and username {username}"
+            )
             username = json.loads(username)
             await sio.enter_room(socket_id, username["userName"])
 
             redis_key = f"room_id:{socket_id}"
-            initial_arr_length = redis_init.execute_command('JSON.ARRLEN', redis_key, '.members')
-            redis_init.execute_command('JSON.ARRAPPEND', redis_key, '.members', json.dumps(username["userName"]))
-            updated_arr_length = redis_init.execute_command('JSON.ARRLEN', redis_key, '.members')
-            logger.info(f"Increased members array length from {initial_arr_length} to {updated_arr_length}")
+            initial_arr_length = redis_init.execute_command(
+                "JSON.ARRLEN", redis_key, ".members"
+            )
+            redis_init.execute_command(
+                "JSON.ARRAPPEND",
+                redis_key,
+                ".members",
+                json.dumps(username["userName"]),
+            )
+            updated_arr_length = redis_init.execute_command(
+                "JSON.ARRLEN", redis_key, ".members"
+            )
+            logger.info(
+                f"Increased members array length from {initial_arr_length} to {updated_arr_length}"
+            )
             if initial_arr_length < updated_arr_length:
-                user = json.loads(redis_init.execute_command('JSON.GET', redis_key))
+                user = json.loads(redis_init.execute_command("JSON.GET", redis_key))
                 logger.info(f"Updated room details in Redis for key {redis_key}")
                 await sio.emit(
                     "join_room",
@@ -31,13 +45,15 @@ class JoinRoom(SocketEvent):
                         "room_id": socket_id,
                         "creator": user["creator"],
                         "members": user["members"],
-                        "message": f"Successfully joined the room"
+                        "message": f"Successfully joined the room",
                     },
                     room=socket_id,
                 )
                 logger.info(f"Emitted join_room event for room_id {socket_id}")
             else:
-                logger.error(f"Error updating room details in Redis for key {redis_key}")
+                logger.error(
+                    f"Error updating room details in Redis for key {redis_key}"
+                )
                 raise JoinRoomException(
                     msg=f"Error updating room details in Redis for key {redis_key}"
                 )
@@ -48,7 +64,7 @@ class JoinRoom(SocketEvent):
                 {
                     "status": "failure",
                     "room_id": socket_id,
-                    "message": f"Error joining room"
+                    "message": f"Error joining room",
                 },
                 room=socket_id,
             )
