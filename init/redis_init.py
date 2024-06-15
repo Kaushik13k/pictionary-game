f6c0a4b5d2a53ac7c6106211dde4b773e81d72ad
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-06-15 17:19:01 +0530

    space added

--- init/redis_init.py
+++ init/redis_init.py
@@ -5,7 +5,7 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
-
+  
 redis_init = redis.Redis(
     host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD
 )
