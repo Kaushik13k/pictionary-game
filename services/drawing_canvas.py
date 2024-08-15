1e6e31f1ebaed724bd0f64c86c1b0c327f38a67b
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-08-15 23:36:19 +0530

    optimsed drawaing canvas

--- services/drawing_canvas.py
+++ services/drawing_canvas.py
@@ -1,7 +1,9 @@
 import json
 import logging
+import traceback
 
 from templates.socket_events import SocketEvent
+from enums.socket_operations import SocketOperations
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,10 +15,13 @@ class DrawingCanvas(SocketEvent):
             message = json.loads(message)["message"]
             await manager.broadcast(
                 {
-                    "event": "drawing_canvas",
+                    "event": SocketOperations.CANVAS.value,
                     "value": {"full_canvas_data": message["full_canvas_data"]},
                 },
                 message["sid"],
             )
         except Exception as e:
-            logger.info("ERROR!")
+            logger.error(f"Error in DrawingCanvas")
+            logger.error(e)
+            logger.error(traceback.format_exc())
+            return
